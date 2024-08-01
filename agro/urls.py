17feebf21e9c5a0bf72e58c72509af555c8be316
xstellas2 <m.stella@escolar.ifrn.edu.br>
2024-08-01 07:55:16 -0300

    stella

--- agro/urls.py
+++ agro/urls.py
@@ -4,4 +4,14 @@ from .views import index
 
 urlpatterns = [
     path("", index)
+    path("", agrotoxico)
+    path("", base)
+    path("", deletar_sugestao)
+    path("", editar_sugestoes)
+    path("", formulario)
+    path("", gafanhotos)
+    path("", index)
+    path("", lagartas)
+    path("", listagem_sugestoes)
+    path("", sobre_nos)
 ]
--- agro/views.py
+++ agro/views.py
@@ -2,3 +2,30 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'base.html')
+
+def base(request):
+    return render(request, 'base.html')
+
+def agrotoxico(request):
+    return render(request, 'agrotoxico.html')
+
+def deletar_sugestao(request):
+    return render(request, 'deletar_sugestao.html')
+
+def editar_sugestoes(request):
+    return render(request, 'editar_sugestoes.html')
+
+def formulario(request):
+    return render(request, 'formulario.html')
+
+def gafanhotos(request):
+    return render(request, 'gafanhotos.html')
+
+def lagartas(request):
+    return render(request, 'lagartas.html')
+
+def listagem_sugestoes(request):
+    return render(request, 'listagem_sugestoes.html')
+
+def sobre_nos(request):
+    return render(request, 'sobre_nos.html')
